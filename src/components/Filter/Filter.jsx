9213a5b8e523dccc6613c3c3da876ea4f813dd65
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-08-21 12:42:06 +0200

    corrected code validation

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
-import { Input } from 'components/ContactForm/ContactForm.styled';
 import React from 'react';
+import { Input } from 'components/ContactForm/ContactForm.styled';
 
 export const Filter = ({ value, onChange }) => (
   <Input type="text" value={value} onChange={onChange} placeholder="Search by name" />
